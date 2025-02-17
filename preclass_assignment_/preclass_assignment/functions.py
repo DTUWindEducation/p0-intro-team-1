82de636611f80764d1d65e694b13f008c3898d04
Bea <s232961@dtu.dk>
2025-02-17 15:49:50 +0100

    Collaboration doc

---
+++ preclass_assignment_/preclass_assignment/functions.py
@@ -0,0 +1,13 @@
+#1. write a simple function
+greet('world')
+
+# 2. If/else statements
+goldilocks (145)
+# 3. For loops
+square=square_list([1,4,9])
+# 4. While loops
+print(fibonacci_stop(30))  
+# 5.Logical operators
+x=[-1,2,6,95]
+state=[1,0,0,0]
+clean_pitch(x,state)
